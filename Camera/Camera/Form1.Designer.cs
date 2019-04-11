12a31e842c621bc19c1d2953c0512c7cd1484e1f
1160300907 <lxr09x001@126.com>
2019-04-11 22:03:16 +0800

    queue

---
+++ Camera/Camera/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Camera
+{
+    partial class Test
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Test));
+            this.videPlayer = new AForge.Controls.VideoSourcePlayer();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tscbxCameras = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // videPlayer
+            // 
+            this.videPlayer.Location = new System.Drawing.Point(13, 65);
+            this.videPlayer.Margin = new System.Windows.Forms.Padding(4);
+            this.videPlayer.Name = "videPlayer";
+            this.videPlayer.Size = new System.Drawing.Size(782, 398);
+            this.videPlayer.TabIndex = 0;
+            this.videPlayer.Text = "videPlayer";
+            this.videPlayer.VideoSource = null;
+            this.videPlayer.Click += new System.EventHandler(this.videPlayer_Click);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(103, 25);
+            this.toolStripLabel1.Text = "视频输入设备:";
+            // 
+            // tscbxCameras
+            // 
+            this.tscbxCameras.Name = "tscbxCameras";
+            this.tscbxCameras.Size = new System.Drawing.Size(212, 28);
+            this.tscbxCameras.Click += new System.EventHandler(this.tscbxCameras_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(63, 25);
+            this.toolStripButton1.Text = "上课";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(63, 25);
+            this.toolStripButton2.Text = "下课";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(63, 25);
+            this.toolStripButton3.Text = "拍照";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.tscbxCameras,
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1075, 28);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(888, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(888, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(888, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(816, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "改为上节课";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(944, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "改为下节课";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(816, 384);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 32);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "载入课表";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(944, 384);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 32);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "清除课表";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(63, 25);
+            this.toolStripButton4.Text = "删除";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(816, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "当前课程";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(816, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "下节课";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(816, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "上节课";
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1075, 486);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.videPlayer);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Test";
+            this.Text = "myCamera";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videPlayer;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox tscbxCameras;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Camera/Camera/Form1.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using System.IO;
+
+namespace Camera
+{
+
+    public partial class Test : Form
+    {
+        private bool flag = false;
+        private bool using_flag = false;
+        private bool camera_flag = false;
+        private string[,] ke = new string[7, 8];
+        private string[,] te = new string[7, 8];
+        private FilterInfoCollection videoDevices;
+        private int a = 0;
+        private int d_zh = 0;
+        private int d_ke = 0;
+        private bool testing = false;
+        private bool us = false;
+        private string filepath11, filepath12, filepath21, filepath22, filepath31, filepath32;
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void init()
+        {
+
+         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // 枚举所有视频输入设备
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                foreach (FilterInfo device in videoDevices)
+                {
+                    tscbxCameras.Items.Add(device.Name);
+                }
+
+                tscbxCameras.SelectedIndex = 0;
+            }
+            catch (ApplicationException)
+            {
+                tscbxCameras.Items.Add("No local capture devices");
+                videoDevices = null;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (us || testing)
+            {
+                return;
+            }
+            if (!using_flag)
+            {
+                testing = true;
+            }
+            else
+            {
+                a = 0;
+                us = true;
+                camera_flag = false;
+                //todo
+                int d = (int)DateTime.Now.DayOfWeek;
+                if (d != (d_zh+1)%7)
+                {
+                    d_zh = d - 1;
+                    if (d_zh == -1) d_zh = 6;
+                    d_ke=0;
+                }
+                else
+                {
+                    d_ke++;
+                    while (ke[d_zh, d_ke].Equals("无"))
+                    {
+                        d_ke--;
+                    }
+                }
+                filepath11 = DateTime.Now.ToString("yyyy-MM-dd");
+                filepath12 = te[d_zh, d_ke] + "老师第" + (d_ke+1) + "节" + ke[d_zh, d_ke];
+                filepath21 = ke[d_zh, d_ke];
+                filepath31 = te[d_zh, d_ke]+"老师";
+                filepath22 = DateTime.Now.ToString("yyyy-MM-dd") + te[d_zh, d_ke] +"老师第" + (d_ke+1) + "节";
+                filepath32 = DateTime.Now.ToString("yyyy-MM-dd") + "第" + (d_ke+1) + "节" + ke[d_zh, d_ke];
+                if (d_ke == 0)
+                {
+                    label3.Text = "无";
+                }
+                else
+                {
+                    label3.Text = te[d_zh, d_ke-1] + "老师的" + ke[d_zh, d_ke-1] + "，第" + (d_ke) + "节";
+                }
+                label1.Text = te[d_zh, d_ke] + "老师的" + ke[d_zh, d_ke] + "，第" + (d_ke + 1) + "节";
+                if (ke[d_zh, d_ke+1].Equals("无"))
+                {
+                    label2.Text = "无";
+                }
+                else
+                {
+                    label2.Text = te[d_zh, d_ke+1] + "老师的" + ke[d_zh, d_ke+1] + "，第" + (d_ke+2) + "节";
+                }
+                label1.Show();
+                label2.Show();
+                label3.Show();
+                Console.WriteLine(filepath11 + "\\" + filepath12);
+                if (!Directory.Exists(filepath11))
+                {
+                    Directory.CreateDirectory(filepath11);
+                }
+                if (!Directory.Exists(filepath11 + "\\" + filepath12))
+                {
+                    Directory.CreateDirectory(filepath11 + "\\" + filepath12);
+                }
+                if (!Directory.Exists(filepath21))
+                {
+                    Directory.CreateDirectory(filepath21);
+                }
+                if (!Directory.Exists(filepath21+"\\"+filepath22))
+                {
+                    Directory.CreateDirectory(filepath21+"\\"+filepath22);
+                }
+                if (!Directory.Exists(filepath31))
+                {
+                    Directory.CreateDirectory(filepath31);
+                }
+                if (!Directory.Exists(filepath31+"\\"+filepath32))
+                {
+                    Directory.CreateDirectory(filepath31+"\\"+filepath32);
+                }
+            }
+            CameraConn();
+        }
+
+        //连接摄像头
+        private void CameraConn()
+        {
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[tscbxCameras.SelectedIndex].MonikerString);
+            videoSource.DesiredFrameSize = new Size(1920, 1080);
+            videoSource.DesiredFrameRate = 1;
+            videPlayer.VideoSource = videoSource;
+            videPlayer.Start();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (!camera_flag && Directory.Exists(filepath11+"\\"+filepath12) && !testing)
+            {
+                Directory.Delete(filepath11 + "\\" + filepath12, true);
+                Directory.Delete(filepath21+"\\"+ filepath22, true);
+                Directory.Delete(filepath31+"\\"+ filepath32, true);
+                camera_flag = false;
+            }
+            testing = false;
+            us = false;
+            videPlayer.SignalToStop();
+            videPlayer.WaitForStop();
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label1.Show();
+            label2.Show();
+            label3.Show();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            toolStripButton2_Click(null, null);
+        }
+
+        /// <summary>
+        /// 保存图片
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Image img = new Bitmap(videPlayer.Width, videPlayer.Height);
+            videPlayer.DrawToBitmap((Bitmap)img, new Rectangle(0, 0, videPlayer.Width, videPlayer.Height));
+
+            if(!using_flag)
+            {
+                ;
+            }
+            else
+            {
+                a++;
+                us = true;
+                string filename = filepath11 + "\\" + filepath12 + "\\" + a + ".bmp";
+                img.Save(filename, ImageFormat.Bmp);
+                filename = filepath21 + "\\" + filepath22 + "\\" + a + ".bmp";
+                img.Save(filename, ImageFormat.Bmp);
+                filename = filepath31 + "\\" + filepath32 + "\\" + a + ".bmp";
+                img.Save(filename, ImageFormat.Bmp);
+                camera_flag = true;
+            }
+
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            if (a == 0)
+            {
+                MessageBox.Show("已经没有照片", "警告", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                File.Delete(filepath11 + "\\" + filepath12 + "\\" + a + ".bmp");
+                File.Delete(filepath21 + "\\" + filepath22 + "\\" + a + ".bmp");
+                File.Delete(filepath31 + "\\" + filepath32 + "\\" + a + ".bmp");
+                a--;
+                if (a == 0)
+                {
+                    camera_flag = false;
+                }
+            }
+        }
+
+        private void tscbxCameras_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void videPlayer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!us)
+            {
+                return;
+            }
+            if (a == 0)
+            {
+                d_ke--;
+                if (d_ke == -1)
+                {
+                    MessageBox.Show("没有上一节课", "警告", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    d_ke++;
+                }
+                else
+                {
+                    label2.Text = label1.Text;
+                    label1.Text = label3.Text;
+                    if (d_ke == 0)
+                        label3.Text = "无";
+                    else
+                        label3.Text = te[d_zh, d_ke-1] + "老师的" + ke[d_zh, d_ke-1] + "，第" + (d_ke) + "节";
+                }
+            }
+            else
+            {
+                MessageBox.Show("不能在已经拍照后再更改课表", "警告", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            label1.Show();
+            label2.Show();
+            label3.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!us)
+            {
+                return;
+            }
+            if (a == 0)
+            {
+                d_ke++;
+                if (ke[d_zh,d_ke].Equals("无"))
+                {
+                    MessageBox.Show("没有下一节课", "警告", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    d_ke--;
+                }
+                else
+                {
+                    label3.Text = label1.Text;
+                    label1.Text = label2.Text;
+                    if (ke[d_zh,d_ke+1] == "无")
+                        label2.Text = "无";
+                    else
+                        label2.Text = te[d_zh, d_ke+1] + "老师的" + ke[d_zh, d_ke+1] + "，第" + (d_ke + 2) + "节";
+                }
+            }
+            else
+            {
+                MessageBox.Show("不能在已经拍照后再更改课表", "警告", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            label1.Show();
+            label2.Show();
+            label3.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!testing)
+                using_flag = true;
+            string filepath = "课表.txt";
+            StreamReader sr = new StreamReader(filepath, Encoding.Default);
+            string nextLine = "";
+            int i = 0, j = 0;
+            while ((nextLine = sr.ReadLine()) != null)
+            {
+                if (nextLine.Equals(""))
+                {
+                    ke[i, j] = "无";
+                    te[i, j] = "无";
+                    i++;
+                    j = 0;
+                } 
+                else
+                {
+                    int k = nextLine.IndexOf(" ");
+                    ke[i, j] = nextLine.Substring(0, k);
+                    te[i, j] = nextLine.Substring(k + 1);
+                    j++;
+                }
+            }
+            ke[i, j] = "无";
+            te[i, j] = "无";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!us)
+                using_flag = false;
+            else
+                MessageBox.Show("不能在程序使用时清除课表", "警告", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
